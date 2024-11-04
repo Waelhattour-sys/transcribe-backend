c935a8e51221b686be0d6631f51360f156d9fff9
wael <w.hattour@match.care>
2024-11-04 09:18:46 +0100

    first commit

--- controllers/transcription.js
+++ controllers/transcription.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
 const OpenAI = require('openai');
 
-// Initialize OpenAI client with your API key
 const openai = new OpenAI({
-  apiKey: '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', // Ensure the API key is stored in .env
+  apiKey: process.env.OPENAI_API_KEY
 });
 
-// Function to transcribe audio using OpenAI
 const transcribeAudio = async (filePath) => {
   try {
     console.log('path:',fs.createReadStream(filePath))
@@ -15,7 +13,6 @@ const transcribeAudio = async (filePath) => {
       model: 'whisper-1',
     });
 
-    // Return the transcribed text
     return transcription.text;
   } catch (error) {
     console.error('Error during transcription:', error);
